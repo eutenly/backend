29523c7d4de8b3ed337b53a96218769f9164bb0d
APixelVisuals <apixelvisualsofficial@gmail.com>
2020-08-09 22:43:20 -0700

    Added barebones af user schema

---
+++ database/schema/Users.go
@@ -0,0 +1,8 @@
+package schema
+
+//Users is the schema for the users collection
+type Users struct {
+	ID                  string `bson:"_id"`
+	VoteExpireTimestamp int    `bson:"voteExpireTimestamp"`
+	PatreonTier         int    `bson:"patreonTier"`
+}
