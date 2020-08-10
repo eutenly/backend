5070575b16ac13cef8abe2e8f1bd810587312227
APixelVisuals <apixelvisualsofficial@gmail.com>
2020-08-10 12:31:49 -0700

    Finished users schema

--- database/schemas/Users.go
+++ database/schemas/Users.go
@@ -1,8 +1,29 @@
 package schemas
 
+type token struct {
+	IV             string `bson:"iv,omitempty"`
+	EphemPublicKey string `bson:"ephemPublicKey,omitempty"`
+	CipherText     string `bson:"cipherText,omitempty"`
+	Mac            string `bson:"mac,omitempty"`
+}
+
+type connection struct {
+	ID           string `bson:"id,omitempty"`
+	AccessToken  token  `bson:"accessToken,omitempty"`
+	RefreshToken token  `bson:"refreshToken,omitempty"`
+	AccessSecret token  `bson:"accessSecret,omitempty"`
+}
+
+type savedLink struct {
+	URL  string `bson:"url,omitempty"`
+	Note string `bson:"note,omitempty"`
+}
+
 //Users is the schema for the users collection
 type Users struct {
-	ID                  string `bson:"_id"`
-	VoteExpireTimestamp int    `bson:"voteExpireTimestamp"`
-	PatreonTier         int    `bson:"patreonTier"`
+	ID                  string                `bson:"_id,omitempty"`
+	Connections         map[string]connection `bson:"connections,omitempty"`
+	SavedLinks          map[string]savedLink  `bson:"savedLinks,omitempty"`
+	VoteExpireTimestamp int                   `bson:"voteExpireTimestamp,omitempty"`
+	PatreonTier         int                   `bson:"patreonTier,omitempty"`
 }
