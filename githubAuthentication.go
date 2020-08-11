c1eb7561ef8b17677beb711b9f5475e1e730bce0
Maia <zgovorovski@gmail.com>
2020-08-11 01:39:11 +0100

    GitHub Auth now fetches token

--- githubAuthentication.go
+++ githubAuthentication.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo-contrib/session"
+	"golang.org/x/oauth2"
 )
 
 func githubAuthenticationRoutes(e *echo.Echo) {
@@ -26,6 +28,41 @@ func githubAuthenticationRoutes(e *echo.Echo) {
 		return c.Redirect(http.StatusTemporaryRedirect, authURL)
 	})
 	e.GET("/auth/github", func(c echo.Context) error {
-		return c.String(http.StatusForbidden, "not ready")
+		//Get OAUTH2 login code
+		authCode := c.QueryParam("code")
+
+		//If no token was passed then error
+		if authCode == "" {
+			return c.String(http.StatusUnauthorized, "A Discord login error occured.")
+		}
+
+		//Request accessToken
+		accessToken, err := authenticateGitHub(authCode)
+		if err != nil {
+			return c.String(http.StatusUnauthorized, "A Github login error occured. "+err.Error())
+		}
+
+		return c.String(http.StatusOK, "access token: "+accessToken)
 	})
+}
+
+func authenticateGitHub(authCode string) (accessToken string, returnErr error) {
+	conf := &oauth2.Config{
+		ClientID:     os.Getenv("GITHUB_CLIENT_ID"),
+		ClientSecret: os.Getenv("GITHUB_CLIENT_SECRET"),
+		Scopes:       []string{""},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://github.com/login/oauth/authorize",
+			TokenURL: "https://github.com/login/oauth/access_token",
+		},
+	}
+
+	ctx := context.Background()
+	tok, err := conf.Exchange(ctx, authCode)
+	if err != nil {
+		returnErr = err
+		return
+	}
+	return tok.AccessToken, err
+
 }
