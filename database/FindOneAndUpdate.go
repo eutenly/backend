24562d1ea1ac8e5f2caf6aff8dffdacd2dcb380b
APixelVisuals <apixelvisualsofficial@gmail.com>
2020-08-12 13:53:20 -0700

    Added upsert support for `FindOneAndUpdate` db function

--- database/FindOneAndUpdate.go
+++ database/FindOneAndUpdate.go
@@ -2,16 +2,24 @@ package database
 
 import (
 	"context"
+
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 //FindOneAndUpdate updates a document with `filter` to have `data` in `collection`
-func FindOneAndUpdate(collection string, filter interface{}, data map[string]interface{}) (result map[string]interface{}, err error) {
+func FindOneAndUpdate(collection string, filter interface{}, data map[string]interface{}, upsert ...bool) (result map[string]interface{}, err error) {
 
 	//Parse set
 	parseSet(data)
 
+	//Parse options
+	if upsert == nil {
+		upsert = []bool{false}
+	}
+	options := options.FindOneAndUpdate().SetUpsert(upsert[0])
+
 	//Run query
-	err = db.Collection(collection).FindOneAndUpdate(context.Background(), filter, data).Decode(&result)
+	err = db.Collection(collection).FindOneAndUpdate(context.Background(), filter, data, options).Decode(&result)
 	if err != nil {
 		return nil, err
 	}
