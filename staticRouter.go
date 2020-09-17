d99058e1250c416e045167d5524753cea7a89f0d
Maia <zgovorovski@gmail.com>
2020-09-17 13:09:24 +0100

    Static router supports SPAs

--- staticRouter.go
+++ staticRouter.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func staticRouter(e *echo.Echo) {
-	e.Static("/", "static");
+	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
+        Root:   "static",
+        Index: "index.html",
+        Browse: true,
+        HTML5:  true,
+    }))
 }
