d64b27d67cb8c032139ea14afceb825881975dfc
Maia <zgovorovski@gmail.com>
2020-10-20 21:33:36 +0100

    Fixed static routing

--- staticRouter.go
+++ staticRouter.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
 )
 
 func staticRouter(e *echo.Echo) {
-	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
-        Root:   "static",
-        Index: "index.html",
-        Browse: true,
-        HTML5:  true,
-    }))
+	e.Static("/", "static")
 }
