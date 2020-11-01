722c36b5f0276d8c13935481bef48bdb7f22a4dc
Maia <zgovorovski@gmail.com>
2020-11-01 09:55:28 +0000

    Added memberCount to Geola endpoint

--- api/geola.go
+++ api/geola.go
@@ -2,7 +2,9 @@ package api
 
 import (
 	"../influxdb"
+	"fmt"
 	"github.com/labstack/echo"
+	"github.com/sirupsen/logrus"
 	"os"
 )
 
@@ -11,14 +13,32 @@ func geolaRoutes(app *echo.Echo) {
 		if c.Request().Header.Get("Authorization") != os.Getenv("GEOLA_ENDPOINT_KEY") {
 			return c.NoContent(401)
 		}
-		influxdb.CollectStat("eutenland_join_leave", map[string]string{"type": "join"})
+
+		//Get body
+		var body map[string]interface{} = make(map[string]interface{})
+		err := c.Bind(&body)
+		if c.Bind(&body) != nil {
+			logrus.Error(err)
+			return c.String(500, err.Error())
+		}
+
+		influxdb.CollectStat("eutenland_join_leave", map[string]string{"type": "join"}, map[string]interface{}{"totalMembers": fmt.Sprint(body["memberCount"])})
 		return c.NoContent(200)
 	})
 	app.POST("/api/v1/geola/leave", func(c echo.Context) error {
 		if c.Request().Header.Get("Authorization") != os.Getenv("GEOLA_ENDPOINT_KEY") {
 			return c.NoContent(401)
 		}
-		influxdb.CollectStat("eutenland_join_leave", map[string]string{"type": "leave"})
+
+		//Get body
+		var body map[string]interface{} = make(map[string]interface{})
+		err := c.Bind(&body)
+		if c.Bind(&body) != nil {
+			logrus.Error(err)
+			return c.String(500, err.Error())
+		}
+
+		influxdb.CollectStat("eutenland_join_leave", map[string]string{"type": "leave"}, map[string]interface{}{"totalMembers": fmt.Sprint(body["memberCount"])})
 		return c.NoContent(200)
 	})
 }
