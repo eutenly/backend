3b86e58575a869740a246ffb0500498d8e9a96e2
Maia <zgovorovski@gmail.com>
2020-11-01 17:31:57 +0000

    Fixed Geola's endpoint

--- api/geola.go
+++ api/geola.go
@@ -2,11 +2,14 @@ package api
 
 import (
 	"../influxdb"
-	"fmt"
 	"github.com/labstack/echo"
 	"github.com/sirupsen/logrus"
 	"os"
 )
+
+type geolaRequest struct {
+	MemberCount string `json:"memberCount"`
+}
 
 func geolaRoutes(app *echo.Echo) {
 	app.POST("/api/v1/geola/join", func(c echo.Context) error {
@@ -15,14 +18,14 @@ func geolaRoutes(app *echo.Echo) {
 		}
 
 		//Get body
-		var body map[string]interface{} = make(map[string]interface{})
+		var body geolaRequest
 		err := c.Bind(&body)
-		if c.Bind(&body) != nil {
-			logrus.Error(err)
+		if err != nil {
+			logrus.Error("Cannot assign body", err)
 			return c.String(500, err.Error())
 		}
 
-		influxdb.CollectStat("eutenland_join_leave", map[string]string{"type": "join"}, map[string]interface{}{"totalMembers": fmt.Sprint(body["memberCount"])})
+		influxdb.CollectStat("eutenland_join_leave", map[string]string{"type": "join"}, map[string]interface{}{"totalMembers": body.MemberCount})
 		return c.NoContent(200)
 	})
 	app.POST("/api/v1/geola/leave", func(c echo.Context) error {
@@ -31,14 +34,14 @@ func geolaRoutes(app *echo.Echo) {
 		}
 
 		//Get body
-		var body map[string]interface{} = make(map[string]interface{})
+		var body geolaRequest
 		err := c.Bind(&body)
-		if c.Bind(&body) != nil {
-			logrus.Error(err)
+		if err != nil {
+			logrus.Error("Cannot assign body", err)
 			return c.String(500, err.Error())
 		}
 
-		influxdb.CollectStat("eutenland_join_leave", map[string]string{"type": "leave"}, map[string]interface{}{"totalMembers": fmt.Sprint(body["memberCount"])})
+		influxdb.CollectStat("eutenland_join_leave", map[string]string{"type": "leave"}, map[string]interface{}{"totalMembers": body.MemberCount})
 		return c.NoContent(200)
 	})
 }
